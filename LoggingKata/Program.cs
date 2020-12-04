7ae85da46b56c5ce267af33e35e01635e3c9ba00
solidponcho <42821325+solidponcho@users.noreply.github.com>
2020-12-03 18:46:35 -0600

    finished tacoparser

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -2,12 +2,14 @@
 using System.Linq;
 using System.IO;
 using GeoCoordinatePortable;
+using System.Threading;
 
 namespace LoggingKata
 {
     class Program
     {
         static readonly ILog logger = new TacoLogger();
+        private static int i;
         const string csvPath = "TacoBell-US-AL.csv";
 
         static void Main(string[] args)
@@ -32,28 +34,91 @@ namespace LoggingKata
             // DON'T FORGET TO LOG YOUR STEPS
 
             // Now that your Parse method is completed, START BELOW ----------
+            ITrackable firstTB = null;
+            ITrackable secondTB = null;
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
             // Create a `double` variable to store the distance
 
+            double distanceBetween = 0;
+
+            var geoA = new GeoCoordinate();
+
+            var geoB = new GeoCoordinate();
+
+
+
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
+
             //HINT NESTED LOOPS SECTION---------------------
+
+            Console.WriteLine("Calculating Distances");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.WriteLine();
+
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+            for (int i = 0; i < locations.Length - 1; i++)
+            {
+                var locA = locations[i];
+                // Create a new corA Coordinate with your locA's lat and long
+                geoA.Latitude = locA.Location.Latitude;
+                geoA.Longitude = locA.Location.Longitude;
 
-            // Create a new corA Coordinate with your locA's lat and long
+                // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                for (int a = i + 1; a < locations.Length; a++)
+                {
 
-            // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                    // Create a new Coordinate with your locB's lat and long
+                    var locB = locations[a];
+                    geoB.Latitude = locB.Location.Latitude;
+                    geoB.Longitude = locB.Location.Longitude;
 
-            // Create a new Coordinate with your locB's lat and long
+                    // Now, compare the two using `.GetDistanceTo()`, which returns a double
+                    var newDistance = geoA.GetDistanceTo(geoB);
+                    // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
+                    if (newDistance > distanceBetween)
+                    {
+                        distanceBetween = newDistance;
+                        firstTB = locA;
+                        secondTB = locB;
+                    }
 
-            // Now, compare the two using `.GetDistanceTo()`, which returns a double
-            // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
+                }
+            }
+
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
+            var ans = distanceBetween * 0.00062137;
+            ans = Math.Round(ans, 2);
+            logger.LogInfo("Converting to miles");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.WriteLine();
+
+            Console.WriteLine($"{firstTB.Name} and {secondTB.Name} are the two farthest apart, with a difference in distance of {ans} miles");
+
+            Console.WriteLine("Press Enter to close...");
+            Console.ReadLine();
 
 
-            
+
+
+
+
         }
     }
 }
+
+
